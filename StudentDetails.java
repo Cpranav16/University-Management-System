5619dc2fe6cdcec6907c6b508fc981b647cf60f2
PRANAV <pranavchavan815@gmail.com>
2025-04-03 23:34:14 +0530

    Changes

--- StudentDetails.java
+++ StudentDetails.java
@@ -1,10 +1,17 @@
 import javax.swing.*;
 
 public class StudentDetails extends JFrame {
+    JLabel lb_name, lb_rno, lb_address, lb_email, lb_classxii, lb_course, lb_fathername, lb_dob, lb_phone, lb_classx, lb_aadhar, lb_branch;
+    JTextField tf_name, tf_rno, tf_address, tf_emial, tf_classxii, tf_course, tf_fathername, tf_dob, tf_phone, tf_classx, tf_aadhar, tf_branch;
+    JButton submit, cancel;
 
     StudentDetails(){
+
         setSize(850,750);
         setLocation(300,37);
+
+        //Name
+        lb_name = new JTextField();
         setVisible(true);
     }
 
